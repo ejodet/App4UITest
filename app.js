0e9b212d4ae81729d71237f621453c0bd46cb23b
UserMalmo <malmoexecution@gmail.com>
2017-01-04 18:46:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cfenv = require('cfenv');
 var app = express();
 app.use(app.router);
 app.use(express.errorHandler());
@@ -6,7 +7,7 @@ app.set('view engine', 'jade');
 app.get('/', function(req, res){
 	res.render('index');
 });
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-var port = (process.env.VCAP_APP_PORT || 3000);
-app.listen(port, host);
-console.log('App started on port ' + port);
+var appEnv = cfenv.getAppEnv();
+app.listen(appEnv.port, '0.0.0.0', function() {
+  console.log("server starting on " + appEnv.url);
+});
